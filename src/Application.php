da1571012997647d2e4af92890b62574a82228a1
Dmitry Patsura <talk@dmtry.me>
2015-06-20 17:41:33 +0900

    Get application version from const and + cv hash

--- src/Application.php
+++ src/Application.php
@@ -9,10 +9,19 @@ class Application extends \Symfony\Component\Console\Application
 {
     protected $finder;
 
+    const VERSION = '0.0.1-dev';
+
     public function __construct()
     {
-        parent::__construct('PHP Static Analyzer', '0.0.1-dev');
+        parent::__construct('PHP Static Analyzer', self::VERSION . ' #' . $this->getCVVersion());
 
         $this->add(new \PHPSA\Command\CheckCommand());
+    }
+
+    protected function getCVVersion()
+    {
+        exec('git describe --always', $version_mini_hash);
+
+        return $version_mini_hash ? $version_mini_hash[0] : '';
     }
 }
