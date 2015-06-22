394d452d741c4f569c1a8cfa37742e583f3cad9e
Dmitry Patsura <talk@dmtry.me>
2015-06-22 23:30:44 +0900

    Allow to use option blame

--- src/Command/CheckCommand.php
+++ src/Command/CheckCommand.php
@@ -20,8 +20,10 @@ use PhpParser\Node;
 use PhpParser\Parser;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CheckCommand extends Command
@@ -31,9 +33,8 @@ class CheckCommand extends Command
         $this
             ->setName('check')
             ->setDescription('SPA')
-            ->setDefinition(array(
-                new InputArgument('path', InputArgument::OPTIONAL, 'Path to check file or directory', '.'),
-            ));
+            ->addOption('blame', null, InputOption::VALUE_OPTIONAL, 'Git blame author for bad code ;)', false)
+            ->addArgument('path', InputArgument::OPTIONAL, 'Path to check file or directory', '.');
     }
 
     /**
