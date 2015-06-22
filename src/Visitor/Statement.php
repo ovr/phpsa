e1cd4fef025abf2d7a85f302db745f19268589c3
Dmitry Patsura <talk@dmtry.me>
2015-06-22 17:33:27 +0900

    Refactoring for Visitor\Statement

--- src/Visitor/Statement.php
+++ src/Visitor/Statement.php
@@ -5,23 +5,32 @@
 
 namespace PHPSA\Visitor;
 
+use PHPSA\Context;
+use PhpParser\Node;
+
 class Statement
 {
-    protected function passReturn(\PhpParser\Node\Stmt\Return_ $st, $context)
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    protected function passReturn(Node\Stmt\Return_ $st)
     {
-        $expression = new Expression($st->expr, $context);
+        $expression = new Expression($st->expr, $this->context);
         $compiledExpression = $expression->compile($st->expr);
     }
 
-    public function __construct(\PhpParser\Node\Stmt $st, $context)
+    public function __construct(Node\Stmt $stmt, Context $context)
     {
-        switch (get_class($st)) {
+        $this->context = $context;
+
+        switch (get_class($stmt)) {
             case 'PhpParser\Node\Stmt\Return_':
-                $this->passReturn($st, $context);
+                $this->passReturn($stmt);
                 break;
             default:
-                var_dump(get_class($st));
-//                var_dump($st);
+                var_dump(get_class($stmt));
                 break;
         }
     }
