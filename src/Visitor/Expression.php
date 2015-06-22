f27013473f8387c72f1bb27b9487122476636d72
Dmitry Patsura <talk@dmtry.me>
2015-06-22 19:38:00 +0900

    Visitor\Expression - passBinaryOpPlus

--- src/Visitor/Expression.php
+++ src/Visitor/Expression.php
@@ -316,6 +316,24 @@ class Expression
         $expression = new Expression($expr->right, $this->context);
         $right = $expression->compile($expr->right);
 
+        switch ($left->getType()) {
+            case CompiledExpression::LNUMBER:
+            case CompiledExpression::DNUMBER:
+                switch ($right->getType()) {
+                    case CompiledExpression::LNUMBER:
+                    case CompiledExpression::DNUMBER:
+                        return new CompiledExpression(CompiledExpression::DNUMBER, $left->getValue() + $right->getValue());
+                        break;
+                    default:
+                        //
+                        break;
+                }
+                break;
+            default:
+                //
+                break;
+        }
+
         return new CompiledExpression(CompiledExpression::UNKNOWN);
     }
 
