e958778a4d7cc0d6f1230cb7ca19d10da4ad10e6
Dmitry Patsura <talk@dmtry.me>
2015-06-22 13:09:18 +0900

    Added simple test for simple vars

---
+++ tests/simple/tests/1.php
@@ -0,0 +1,42 @@
+<?php
+
+class PassVariable
+{
+  public function returnInt()
+  {
+    return 1;
+  }
+
+  public function returnFloat()
+  {
+    return 1.5;
+  }
+
+  public function returnBoolTrue()
+  {
+    return true;
+  }
+
+  public function returnBoolFalse()
+  {
+    return false;
+  }
+
+  public function returnString()
+  {
+    return "test string";
+  }
+
+  public function returnEmptyArray()
+  {
+    return array();
+  }
+
+  public function returnExampleArray()
+  {
+    return array(
+      1 => "test",
+      2 => "string"
+    );
+  }
+}
