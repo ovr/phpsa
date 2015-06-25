64a7123fbba8acad8c3f3c965811a9dbcf92e7cc
Dmitry Patsura <talk@dmtry.me>
2015-06-25 16:33:02 +0900

    Added docblocks for CompiledExpression

--- src/CompiledExpression.php
+++ src/CompiledExpression.php
@@ -7,26 +7,65 @@ namespace PHPSA;
 
 class CompiledExpression
 {
+    /**
+     * Unknown type
+     */
     const UNKNOWN = 0;
 
+    /**
+     * Int
+     */
     const LNUMBER = 1;
 
+    /**
+     * Float
+     */
     const DNUMBER = 2;
 
+    /**
+     * String
+     */
     const STRING = 3;
 
+    /**
+     * Boolean
+     * true or false
+     */
     const BOOLEAN = 4;
 
+    /**
+     * Array
+     */
     const ARR = 5;
 
+    /**
+     * Object
+     */
     const OBJECT = 6;
 
+    /**
+     * Resource handler
+     */
     const RESOURCE = 7;
 
+    /**
+     * By default we dont know what it is
+     *
+     * @var int
+     */
     protected $type = self::UNKNOWN;
 
+    /**
+     * Possible value
+     *
+     * @var mixed
+     */
     protected $value;
 
+    /**
+     * @param $type
+     * @param null $value
+     */
     public function __construct($type, $value = null)
     {
         $this->type = $type;
