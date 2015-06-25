2d8028e7dce5cf2d7772f0a283a7515db8813ae0
Dmitry Patsura <talk@dmtry.me>
2015-06-25 22:58:59 +0900

    Implement simple pass for Stmt\If_ - true branch

--- src/Visitor/Statement.php
+++ src/Visitor/Statement.php
@@ -21,6 +21,28 @@ class Statement
         $compiledExpression = $expression->compile($st->expr);
     }
 
+    /**
+     * @param Node\Stmt\If_ $st
+     */
+    public function passIf(Node\Stmt\If_ $st)
+    {
+        $expression = new Expression($st->cond, $this->context);
+        $compiledExpression = $expression->compile($st->cond);
+
+        if (count($st->stmts) > 0) {
+            foreach ($st->stmts as $st) {
+                if ($st instanceof Node\Stmt) {
+                    $expr = new Statement($st, $this->context);
+                } else {
+                    $expr = new Expression($st, $this->context);
+                    $expr->compile($st);
+                }
+            }
+        } else {
+
+        }
+    }
+
     public function __construct(Node\Stmt $stmt, Context $context)
     {
         $this->context = $context;
@@ -28,6 +50,9 @@ class Statement
         switch (get_class($stmt)) {
             case 'PhpParser\Node\Stmt\Return_':
                 $this->passReturn($stmt);
+                break;
+            case 'PhpParser\Node\Stmt\If_':
+                $this->passIf($stmt);
                 break;
             default:
                 $this->context->debug('Unknown statement: ' . get_class($stmt));
