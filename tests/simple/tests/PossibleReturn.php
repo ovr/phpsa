6926e53f9dad8a98faaac08a5213b613838b9e36
Dmitry Patsura <talk@dmtry.me>
2015-06-25 22:54:02 +0900

    [Tests] PossibleReturn with ifs

--- tests/simple/tests/PossibleReturn.php
+++ tests/simple/tests/PossibleReturn.php
@@ -4,41 +4,68 @@ namespace Tests\Simple\Tests;
 
 class PossibleReturn
 {
-  public function returnInt()
-  {
-    return 1;
-  }
+    public function returnInt()
+    {
+        return 1;
+    }
 
-  public function returnFloat()
-  {
-    return 1.5;
-  }
+    public function returnFloat()
+    {
+        return 1.5;
+    }
 
-  public function returnBoolTrue()
-  {
-    return true;
-  }
+    public function returnBoolTrue()
+    {
+        return true;
+    }
 
-  public function returnBoolFalse()
-  {
-    return false;
-  }
+    public function returnBoolFalse()
+    {
+        return false;
+    }
 
-  public function returnString()
-  {
-    return "test string";
-  }
+    public function returnString()
+    {
+        return "test string";
+    }
 
-  public function returnEmptyArray()
-  {
-    return array();
-  }
+    public function returnEmptyArray()
+    {
+        return array();
+    }
 
-  public function returnExampleArray()
-  {
-    return array(
-      1 => "test",
-      2 => "string"
-    );
-  }
+    public function returnExampleArray()
+    {
+        return array(
+            1 => "test",
+            2 => "string"
+        );
+    }
+
+    public function simpleIf($a)
+    {
+        if ($a) {
+            return $a;
+        }
+
+        return false;
+    }
+
+    public function simpleIf2($a)
+    {
+        if (!$a) {
+            return $a;
+        }
+
+        return true;
+    }
+
+    public function simpleIf3($a)
+    {
+        if ($a === 3) {
+            return true;
+        }
+
+        return true;
+    }
 }
