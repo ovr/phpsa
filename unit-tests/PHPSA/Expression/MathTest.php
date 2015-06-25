f7ae8775710ab8cc87848c4e430dd53c6bc6d481
Dmitry Patsura <talk@dmtry.me>
2015-06-25 18:33:37 +0900

    [Unit-tests] Added more tests for BinaryOpPlus expression

--- unit-tests/PHPSA/Expression/MathTest.php
+++ unit-tests/PHPSA/Expression/MathTest.php
@@ -11,6 +11,17 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 
 class MathTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @return \PHPSA\Context
+     */
+    protected function getContext()
+    {
+        $context = new \PHPSA\Context(new ConsoleOutput(), new Application());
+        $context->setScope(new ClassDefinition('MathTest'));
+
+        return $context;
+    }
+
     public function testPlusIntToInt()
     {
         $baseExpression = new Node\Expr\BinaryOp\Plus(
@@ -18,14 +29,56 @@ class MathTest extends \PHPUnit_Framework_TestCase
             new Node\Scalar\LNumber(1)
         );
 
-        $context = new \PHPSA\Context(new ConsoleOutput(), new Application());
-        $context->setScope(new ClassDefinition('MathTest'));
-
-        $visitor = new Expression($baseExpression, $context);
+        $visitor = new Expression($baseExpression, $this->getContext());
         $compiledExpression = $visitor->compile($baseExpression);
 
         $this->assertInstanceOf('PHPSA\CompiledExpression', $compiledExpression);
-//        $this->assertSame(CompiledExpression::LNUMBER, $compiledExpression->getType());
+        $this->assertSame(CompiledExpression::LNUMBER, $compiledExpression->getType());
         $this->assertSame(2, $compiledExpression->getValue());
+    }
+
+    public function testPlusIntToFloat()
+    {
+        $baseExpression = new Node\Expr\BinaryOp\Plus(
+            new Node\Scalar\LNumber(1),
+            new Node\Scalar\DNumber(1.5)
+        );
+
+        $visitor = new Expression($baseExpression, $this->getContext());
+        $compiledExpression = $visitor->compile($baseExpression);
+
+        $this->assertInstanceOf('PHPSA\CompiledExpression', $compiledExpression);
+        $this->assertSame(CompiledExpression::DNUMBER, $compiledExpression->getType());
+        $this->assertSame(2.5, $compiledExpression->getValue());
+    }
+
+    public function testPlusFloatToInt()
+    {
+        $baseExpression = new Node\Expr\BinaryOp\Plus(
+            new Node\Scalar\DNumber(1.5),
+            new Node\Scalar\LNumber(1)
+        );
+
+        $visitor = new Expression($baseExpression, $this->getContext());
+        $compiledExpression = $visitor->compile($baseExpression);
+
+        $this->assertInstanceOf('PHPSA\CompiledExpression', $compiledExpression);
+        $this->assertSame(CompiledExpression::DNUMBER, $compiledExpression->getType());
+        $this->assertSame(2.5, $compiledExpression->getValue());
+    }
+
+    public function testPlusFloatToFloat()
+    {
+        $baseExpression = new Node\Expr\BinaryOp\Plus(
+            new Node\Scalar\DNumber(1.5),
+            new Node\Scalar\LNumber(1.5)
+        );
+
+        $visitor = new Expression($baseExpression, $this->getContext());
+        $compiledExpression = $visitor->compile($baseExpression);
+
+        $this->assertInstanceOf('PHPSA\CompiledExpression', $compiledExpression);
+        $this->assertSame(CompiledExpression::DNUMBER, $compiledExpression->getType());
+        $this->assertSame(3.0, $compiledExpression->getValue());
     }
 }
