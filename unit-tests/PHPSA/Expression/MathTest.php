4da0a253f1ddcff397d1c29ce0621a28020e624b
Dmitry Patsura <talk@dmtry.me>
2015-06-25 18:19:18 +0900

    [Unit-tests] Added first tests on Math - 1+1

--- unit-tests/PHPSA/Expression/MathTest.php
+++ unit-tests/PHPSA/Expression/MathTest.php
@@ -2,10 +2,30 @@
 
 namespace Tests\PHPSA\Expression;
 
+use PhpParser\Node;
+use PHPSA\Application;
+use PHPSA\CompiledExpression;
+use PHPSA\Definition\ClassDefinition;
+use PHPSA\Visitor\Expression;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
 class MathTest extends \PHPUnit_Framework_TestCase
 {
     public function testPlusIntToInt()
     {
-        $this->assertTrue(true);
+        $baseExpression = new Node\Expr\BinaryOp\Plus(
+            new Node\Scalar\LNumber(1),
+            new Node\Scalar\LNumber(1)
+        );
+
+        $context = new \PHPSA\Context(new ConsoleOutput(), new Application());
+        $context->setScope(new ClassDefinition('MathTest'));
+
+        $visitor = new Expression($baseExpression, $context);
+        $compiledExpression = $visitor->compile($baseExpression);
+
+        $this->assertInstanceOf('PHPSA\CompiledExpression', $compiledExpression);
+//        $this->assertSame(CompiledExpression::LNUMBER, $compiledExpression->getType());
+        $this->assertSame(2, $compiledExpression->getValue());
     }
 }
