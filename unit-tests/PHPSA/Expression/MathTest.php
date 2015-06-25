ecc6ead02b50549f63e27f6fa545a8446f9299e6
Dmitry Patsura <talk@dmtry.me>
2015-06-25 18:12:27 +0900

    Added skeleton for Unit-tests

---
+++ unit-tests/PHPSA/Expression/MathTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\PHPSA\Expression;
+
+class MathTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPlusIntToInt()
+    {
+        $this->assertTrue(true);
+    }
+}
