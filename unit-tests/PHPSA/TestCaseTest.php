b3f909d60397be7b3afd8a2a624e9713d8a2a3d1
Dmitry Patsura <talk@dmtry.me>
2015-06-27 14:08:13 +0900

    [Unit-tests] Added test on TestCase newScalarExpr for int, double

--- unit-tests/PHPSA/TestCaseTest.php
+++ unit-tests/PHPSA/TestCaseTest.php
@@ -9,4 +9,34 @@ class TestCaseTest extends TestCase
         $result = $this->getContext();
         $this->assertInstanceOf('PHPSA\Context', $result);
     }
+
+    public function testNewScalarExprInt()
+    {
+        $scalar = $this->newScalarExpr(-1);
+        $this->assertInstanceOf('PHPParser\Node\Scalar\LNumber', $scalar);
+        $this->assertSame(-1, $scalar->value);
+
+        $scalar = $this->newScalarExpr(0);
+        $this->assertInstanceOf('PHPParser\Node\Scalar\LNumber', $scalar);
+        $this->assertSame(0, $scalar->value);
+
+        $scalar = $this->newScalarExpr(1);
+        $this->assertInstanceOf('PHPParser\Node\Scalar\LNumber', $scalar);
+        $this->assertSame(1, $scalar->value);
+    }
+
+    public function testNewScalarExprDouble()
+    {
+        $scalar = $this->newScalarExpr(-1.0);
+        $this->assertInstanceOf('PHPParser\Node\Scalar\DNumber', $scalar);
+        $this->assertSame(-1.0, $scalar->value);
+
+        $scalar = $this->newScalarExpr(0.0);
+        $this->assertInstanceOf('PHPParser\Node\Scalar\DNumber', $scalar);
+        $this->assertSame(0.0, $scalar->value);
+
+        $scalar = $this->newScalarExpr(1.0);
+        $this->assertInstanceOf('PHPParser\Node\Scalar\DNumber', $scalar);
+        $this->assertSame(1.0, $scalar->value);
+    }
 }
