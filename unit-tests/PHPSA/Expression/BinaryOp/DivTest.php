efd6a8c510c459c4560da621371a82e82094cab4
Dmitry Patsura <talk@dmtry.me>
2015-06-29 15:17:27 +0900

    [Unit-tests] Added first test on Div ~ {int} / {int} = {int}

---
+++ unit-tests/PHPSA/Expression/BinaryOp/DivTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\PHPSA\Expression\BinaryOp;
+
+use PhpParser\Node;
+use PHPSA\CompiledExpression;
+use PHPSA\Visitor\Expression;
+
+class DivTest extends \Tests\PHPSA\TestCase
+{
+    /**
+     * Data provider for Plus {int} / {int} = {int}
+     *
+     * @return array
+     */
+    public function testDivIntResultDataProvider()
+    {
+        return array(
+            array(-1, -1, 1),
+            array(1, 1, 1),
+            array(25, 25, 1),
+            array(50, 50, 1),
+            array(500, 50, 10),
+            array(5000, 50, 100)
+        );
+    }
+
+    /**
+     * Tests {int} + {int} = {int}
+     *
+     * @dataProvider testDivIntResultDataProvider
+     */
+    public function testDivIntToIntWithIntResult($a, $b, $c)
+    {
+        $this->assertInternalType('int', $a);
+        $this->assertInternalType('int', $b);
+        $this->assertInternalType('int', $c);
+
+        $baseExpression = new Node\Expr\BinaryOp\Div(
+            new Node\Scalar\LNumber($a),
+            new Node\Scalar\LNumber($b)
+        );
+        $compiledExpression = $this->compileExpression($baseExpression);
+
+        $this->assertInstanceOf('PHPSA\CompiledExpression', $compiledExpression);
+        $this->assertSame(CompiledExpression::LNUMBER, $compiledExpression->getType());
+        $this->assertSame($c, $compiledExpression->getValue());
+    }
+}
