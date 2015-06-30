7822574ff92ac6956c45c90344b40aba8db98075
Dmitry Patsura <talk@dmtry.me>
2015-06-30 16:56:00 +0900

    Added first fixture for next implementation ofr SwitchCase

---
+++ tests/simple/code-smell/SwitchCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Simple\CodeSmell;
+
+/**
+ * Class Cast
+ * @package Tests\Simple\CodeSmell
+ */
+class SwitchCase
+{
+    const A = 1;
+
+    const B = 1;
+
+    const C = 1;
+
+    public function testDuplicationInSwitch($a)
+    {
+        switch ($a) {
+            case 1:
+            //Wrong....
+            case 1:
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+
+                break;
+        }
+    }
+
+    public function testDuplicationInSwitchWithConst($a)
+    {
+        switch ($a) {
+            case self::A:
+                //Wrong....
+            case self::A:
+            case self::B:
+                return true;
+                break;
+            case self::C:
+                return false;
+                break;
+            default:
+
+                break;
+        }
+    }
+}
