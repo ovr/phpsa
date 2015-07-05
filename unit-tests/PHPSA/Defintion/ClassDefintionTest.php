1f60f8a39de4f6de571b27ee4b7bf527c5f819ee
Dmitry Patsura <talk@dmtry.me>
2015-07-05 15:29:45 +0900

    [Tests] ClassDefinition - add tests on hasProperty, addProperty

--- unit-tests/PHPSA/Defintion/ClassDefintionTest.php
+++ unit-tests/PHPSA/Defintion/ClassDefintionTest.php
@@ -10,5 +10,46 @@ class ClassDefintionTest extends TestCase
     {
         $classDefinition = new \PHPSA\Definition\ClassDefinition('MyTestClass');
         $this->assertSame('MyTestClass', $classDefinition->getName());
+
+        return $classDefinition;
+    }
+
+    public function testSetGetHasForClassProperty()
+    {
+        $classDefinition = $this->testSimpleInstance();
+        $this->assertFalse($classDefinition->hasProperty('test1'));
+        $this->assertFalse($classDefinition->hasProperty('test2'));
+
+        $property = new \PhpParser\Node\Stmt\Property(
+            0,
+            array(
+                new \PhpParser\Node\Stmt\PropertyProperty(
+                    'test1',
+                    new \PhpParser\Node\Scalar\String_(
+                        'test string'
+                    )
+                )
+            )
+        );
+        $classDefinition->addProperty($property);
+
+        $this->assertTrue($classDefinition->hasProperty('test1'));
+        $this->assertFalse($classDefinition->hasProperty('test2'));
+
+        $property = new \PhpParser\Node\Stmt\Property(
+            0,
+            array(
+                new \PhpParser\Node\Stmt\PropertyProperty(
+                    'test2',
+                    new \PhpParser\Node\Scalar\String_(
+                        'test string'
+                    )
+                )
+            )
+        );
+        $classDefinition->addProperty($property);
+        
+        $this->assertTrue($classDefinition->hasProperty('test1'));
+        $this->assertTrue($classDefinition->hasProperty('test2'));
     }
 }
