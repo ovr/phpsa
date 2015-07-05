fb12b84824933e4f1a8e41acf8e0ffb9b0858675
Dmitry Patsura <talk@dmtry.me>
2015-07-05 16:20:16 +0900

    Fixes, up code quality

--- src/Definition/ClassMethod.php
+++ src/Definition/ClassMethod.php
@@ -22,19 +22,19 @@ class ClassMethod
     /**
      * @var Node\Stmt\ClassMethod
      */
-    protected $st;
+    protected $statement;
 
     /**
      * @param string $name
      * @param Node[] $ast
      * @param integer $type
      */
-    public function __construct($name, $ast, $type, Node\Stmt\ClassMethod $st)
+    public function __construct($name, $ast, $type, Node\Stmt\ClassMethod $statement)
     {
         $this->name = $name;
         $this->ast = $ast;
         $this->type = $type;
-        $this->st = $st;
+        $this->statement = $statement;
     }
 
     /**
@@ -51,11 +51,11 @@ class ClassMethod
      */
     public function compile(Context $context)
     {
-        if ($this->st->getDocComment() === null) {
+        if ($this->statement->getDocComment() === null) {
             return $context->notice(
                 'missing-docblock',
                 sprintf('Missing docblock for %s() method', $this->name),
-                $this->st
+                $this->statement
             );
         }
 
@@ -63,19 +63,19 @@ class ClassMethod
             return $context->notice(
                 'not-implemented-method',
                 sprintf('Method %s() is not implemented', $this->name),
-                $this->st
+                $this->statement
             );
         }
 
-        if (count($this->st->params) > 0) {
+        if (count($this->statement->params) > 0) {
             /** @var  Node\Param $parameter */
-            foreach ($this->st->params as $parameter) {
+            foreach ($this->statement->params as $parameter) {
                 $context->addSymbol($parameter->name);
             }
         }
 
         foreach ($this->ast as $st) {
-            $result = \PHPSA\nodeVisitorFactory($st, $context);
+            \PHPSA\nodeVisitorFactory($st, $context);
         }
     }
 
