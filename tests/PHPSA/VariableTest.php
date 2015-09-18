417b84b20b3fa7c9b430fb5ab02717414ba27a16
Dmitry Patsura <talk@dmtry.me>
2015-09-18 15:27:35 +0900

    Variable - added test on referenced and type, value changes

--- tests/PHPSA/VariableTest.php
+++ tests/PHPSA/VariableTest.php
@@ -21,4 +21,26 @@ class VariableTest extends TestCase
          */
         static::assertSame(1, $variable->getSets());
     }
+
+    public function testReferenceToChange()
+    {
+        /**
+         * $a = 1;
+         * $b = &$a;
+         */
+        $parentVariable = new Variable('a', 1, CompiledExpression::INTEGER);
+        $variable = new Variable('b', $parentVariable->getValue(), $parentVariable->getType());
+
+        static::assertFalse($variable->isReferenced());
+        $variable->setReferencedTo($parentVariable);
+        static::assertTrue($variable->isReferenced());
+
+        /**
+         * $b = 55.00
+         */
+        $variable->modify(CompiledExpression::DOUBLE, 55.00);
+
+        static::assertSame($variable->getValue(), $parentVariable->getValue());
+        static::assertSame($variable->getType(), $parentVariable->getType());
+    }
 }
