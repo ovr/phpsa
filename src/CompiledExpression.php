75062ea058421d8a910e6838e0554a8a05e9e18f
Dmitry Patsura <talk@dmtry.me>
2015-09-23 20:33:30 +0300

    Fix typo!

--- src/CompiledExpression.php
+++ src/CompiledExpression.php
@@ -172,7 +172,7 @@ class CompiledExpression
      * @todo Implement it ;)
      * @return boolean
      */
-    public function isCurrectTypeValue()
+    public function isCorrectType()
     {
         return true;
     }
