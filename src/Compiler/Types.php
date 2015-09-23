dbaca1aa4d4c2ccf83773902ded105cde03e371a
Dmitry Patsura <talk@dmtry.me>
2015-09-23 20:27:51 +0300

    Implement Types::getType to get CompiledExpression type by value

---
+++ src/Compiler/Types.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSA\Compiler;
+
+use PHPSA\CompiledExpression;
+use RuntimeException;
+
+class Types
+{
+    /**
+     * Get type by $value
+     *
+     * @param $value
+     * @return int
+     */
+    static public function getType($value)
+    {
+        $type = gettype($value);
+        switch ($type) {
+            case 'integer':
+                return CompiledExpression::LNUMBER;
+            case 'float':
+            case 'double':
+                return CompiledExpression::DNUMBER;
+            case 'string':
+                return CompiledExpression::STRING;
+            case 'array':
+                return CompiledExpression::ARR;
+            case 'boolean':
+                return CompiledExpression::BOOLEAN;
+            case 'NULL':
+                return CompiledExpression::NULL;
+        }
+
+        //@codeCoverageIgnoreStart
+        throw new RuntimeException("Type '{$type}' is not supported");
+        //@codeCoverageIgnoreEnd
+
+    }
+}
