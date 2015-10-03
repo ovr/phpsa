75e87959ee892ee45ed40bc2db3db9097d4e1b2a
Dmitry Patsura <talk@dmtry.me>
2015-10-03 21:37:12 +0300

    [Compiler] Types - implement getTypeName method

--- src/Compiler/Types.php
+++ src/Compiler/Types.php
@@ -11,6 +11,38 @@ use RuntimeException;
 class Types
 {
     /**
+     * Get name for the $type
+     *
+     * @param $type
+     * @return string
+     */
+    public static function getTypeName($type)
+    {
+        switch ($type) {
+            case CompiledExpression::INTEGER:
+                return 'integer';
+            case CompiledExpression::DOUBLE:
+                return 'double';
+            case CompiledExpression::NUMBER:
+                return 'number';
+            case CompiledExpression::ARR:
+                return 'array';
+            case CompiledExpression::OBJECT;
+                return 'object';
+            case CompiledExpression::RESOURCE:
+                return 'resource';
+            case CompiledExpression::CALLABLE_TYPE:
+                return 'callable';
+            case CompiledExpression::BOOLEAN:
+                return 'boolean';
+            case CompiledExpression::NULL:
+                return 'null';
+            default:
+                return 'uknown';
+        }
+    }
+
+    /**
      * Get type by $value
      *
      * @param $value
