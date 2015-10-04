3bea7ca2d8d0eb66885e411fa0b0b1d108b0626e
Dmitry Patsura <talk@dmtry.me>
2015-10-04 18:44:57 +0300

    [Compiler] Implement GlobalVariable

---
+++ src/Compiler/GlobalVariable.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSA\Compiler;
+
+use PHPSA\CompiledExpression;
+use PHPSA\Variable;
+
+class GlobalVariable extends Variable
+{
+    /**
+     * @param string $name
+     * @param null $defaultValue
+     * @param int $type
+     */
+    public function __construct($name, $defaultValue = null, $type = CompiledExpression::UNKNOWN)
+    {
+        parent::__construct($name, $defaultValue, $type);
+    }
+
+    /**
+     * @return string
+     */
+    public function getSymbolType()
+    {
+        return 'global-variable';
+    }
+}
