1a27460b3f70f5dd376809ab78a92d17e3b308ca
Dmitry Patsura <talk@dmtry.me>
2015-10-13 23:45:44 +0300

    Added Exception\NotImplementedException

---
+++ src/Exception/NotImplementedException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ovr
+ * Date: 13.10.15
+ * Time: 23:42
+ */
+
+namespace PHPSA\Exception;
+
+class NotImplementedException extends RuntimeException
+{
+
+}
