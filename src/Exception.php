cf62e45e17fdaf3a2cf023decefcc6fcf4c18069
Dmitry Patsura <talk@dmtry.me>
2015-10-13 23:41:16 +0300

    Implement own Exception(s)

---
+++ src/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSA;
+
+use Exception as BaseException;
+
+class Exception extends BaseException
+{
+
+}
---
+++ src/Exception/Runtime.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSA\Exception;
+
+use PHPSA\Exception;
+
+class Runtime extends Exception
+{
+
+}
