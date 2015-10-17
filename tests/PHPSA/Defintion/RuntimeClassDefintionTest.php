07538d673e43901f3572204716b0949686be1071
Patsura Dmitry <talk@dmtry.me>
2015-10-17 16:31:12 +0300

    [Tests] Implement initial test(s) for RuntimeClassDefintion

---
+++ tests/PHPSA/Defintion/RuntimeClassDefintionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\PHPSA\Defintion;
+
+use PHPSA\Definition\RuntimeClassDefinition;
+use ReflectionClass;
+use Tests\PHPSA\TestCase;
+
+class RuntimeClassDefintionTest extends TestCase
+{
+    const TEST_CLASS_NAME = 'PHPSA\Definition\ClassDefinition';
+
+    public function testConstruct()
+    {
+        $definition = new RuntimeClassDefinition(new ReflectionClass(self::TEST_CLASS_NAME));
+        $definition->compile($this->getContext());
+    }
+
+    public function testHasMethod()
+    {
+        $reflection = new ReflectionClass(self::TEST_CLASS_NAME);
+        $definition = new RuntimeClassDefinition($reflection);
+
+        foreach ($reflection->getMethods() as $method) {
+            static::assertTrue($definition->hasMethod($method->getName()));
+        }
+
+        static::assertFalse($definition->hasMethod('XXXXXXXXXXXXXX'));
+    }
+
+    public function testHasConst()
+    {
+        $reflection = new ReflectionClass('PHPSA\Context');
+        $definition = new RuntimeClassDefinition($reflection);
+
+        foreach ($reflection->getConstants() as $constant) {
+            static::assertTrue($definition->hasConst($constant));
+        }
+
+        static::assertFalse($definition->hasConst('XXXXXXXXX'));
+    }
+
+    public function testHasProperty()
+    {
+        $reflection = new ReflectionClass('PHPSA\Context');
+        $definition = new RuntimeClassDefinition($reflection);
+
+        foreach ($reflection->getProperties() as $property) {
+            static::assertTrue($definition->hasProperty($property->getName()));
+        }
+        
+        static::assertFalse($definition->hasProperty('XXXXX'));
+    }
+}
