4c34e61887cbcfbcda6f32361169a17af8cf0ceb
Dmitry Patsura <zaets28rus@gmail.com>
2016-02-06 18:17:09 +0300

    Fix typo

--- src/Context.php
+++ src/Context.php
@@ -149,7 +149,7 @@ class Context
     public function warning($type, $message)
     {
         $filepath = $this->filepath;
-        $this->output->writeln('<comment>Notice:  ' . $message . " in {$filepath}  [{$type}]</comment>");
+        $this->output->writeln('<comment>Warning:  ' . $message . " in {$filepath}  [{$type}]</comment>");
         $this->output->writeln('');
 
         return true;
