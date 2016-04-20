da059e9147fc6caa58a781fdba63f0ad04e078f1
Dmitry Patsura <zaets28rus@gmail.com>
2016-04-20 22:50:17 +0300

    Back to dev

--- src/Application.php
+++ src/Application.php
@@ -24,7 +24,7 @@ class Application extends \Symfony\Component\Console\Application
      */
     public $compiler;
 
-    const VERSION = '0.4.0';
+    const VERSION = '0.4.1-dev';
 
     public function __construct()
     {
