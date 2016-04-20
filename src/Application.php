10bb43b9b9ff55afa3e29b6e0a37cb4bbd39c992
Dmitry Patsura <zaets28rus@gmail.com>
2016-04-20 22:47:24 +0300

    Bump

--- src/Application.php
+++ src/Application.php
@@ -24,7 +24,7 @@ class Application extends \Symfony\Component\Console\Application
      */
     public $compiler;
 
-    const VERSION = '0.4-dev';
+    const VERSION = '0.4.0';
 
     public function __construct()
     {
