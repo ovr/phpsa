2e9f97636ab16473c934ff7ebb10014ad55c9a18
Dmitry Patsura <zaets28rus@gmail.com>
2016-05-21 15:50:57 +0900

    Implement compiling tests on fixtures

---
+++ tests/PHPSA/CompilingFixturesTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\PHPSA;
+
+use PhpParser\ParserFactory;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class CompilingFixturesTes extends TestCase
+{
+    public function provideTestParseAndDump()
+    {
+        $iter = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                __DIR__ . '/../compiling-fixtures'
+            ),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($iter as $file) {
+            if (!$file->isFile()) {
+                continue;
+            }
+
+            $contents = file_get_contents($file);
+            yield $file->getBasename() => explode('----------------------------', $contents);
+        }
+    }
+
+    /** @dataProvider provideTestParseAndDump */
+    public function testParseAndDump($code, $expectedDump)
+    {
+        $parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7, new \PhpParser\Lexer\Emulative(
+            array(
+                'usedAttributes' => array(
+                    'comments',
+                    'startLine',
+                    'endLine',
+                    'startTokenPos',
+                    'endTokenPos'
+                )
+            )
+        ));
+        $ast = $parser->parse($code);
+
+        $expressionCompiler = $this->getContext()->getExpressionCompiler();
+
+        foreach ($ast as $node) {
+            $compiledExpression = $expressionCompiler ->compile($node);
+            self::assertInstanceOfCompiledExpression($compiledExpression);
+        }
+
+        self::assertSame(
+            json_encode($compiledExpression->__debugInfo()),
+            trim($expectedDump)
+        );
+    }
+}
