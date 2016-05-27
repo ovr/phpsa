a354a02976f6978eccd88a780426a1ce452083b5
Dmitry Patsura <zaets28rus@gmail.com>
2016-05-27 23:45:41 +0900

    [Analyzer] Helper\ResolveExpressionTrait - added normal traverser to known about all Return_

--- src/Analyzer/Helper/ResolveExpressionTrait.php
+++ src/Analyzer/Helper/ResolveExpressionTrait.php
@@ -5,6 +5,7 @@
 
 namespace PHPSA\Analyzer\Helper;
 
+use PhpParser\Node;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Stmt\Return_;
 use PHPSA\Context;
@@ -37,11 +38,59 @@ trait ResolveExpressionTrait
      * @param \PhpParser\Node[] $nodes
      * @return \PhpParser\Node\Stmt\Return_
      */
-    private function findReturnStatement(array $nodes)
+    protected function findReturnStatement(array $nodes)
     {
-        foreach ($nodes as $node) {
+        foreach ($this->traverseArray($nodes) as $node) {
             if ($node instanceof Return_) {
                 yield $node;
+            }
+        }
+    }
+
+    /**
+     * For the code above
+     * Я атеист, но когда я начинал это писать, только Бог и я понимали, что я делаю
+     * Сейчас остался только Бог
+     */
+
+    /**
+     * @param Node $node
+     * @return \Generator
+     */
+    protected function traverseNode(Node $node)
+    {
+        foreach ($node->getSubNodeNames() as $name) {
+            $subNode = &$node->$name;
+
+            if (is_array($subNode)) {
+                foreach ($this->traverseArray($subNode) as $rNode) {
+                    yield $rNode;
+                }
+            } elseif ($subNode instanceof Node) {
+                yield $node;
+
+                foreach ($this->traverseNode($subNode) as $rNode) {
+                    yield $rNode;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param array $nodes
+     * @return \Generator
+     */
+    protected function traverseArray(array $nodes)
+    {
+        foreach ($nodes as $node) {
+            if (is_array($node)) {
+                foreach ($this->traverseArray($node) as $rNode) {
+                    yield $rNode;
+                }
+            } elseif ($node instanceof Node) {
+                foreach ($this->traverseNode($node) as $rNode) {
+                    yield $rNode;
+                }
             }
         }
     }
