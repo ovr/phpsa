a218fdf946f05b08f87a1d3559f082360cadbadf
Enrico <eddmler@gmail.com>
2016-05-30 15:51:44 +0200

    Add more alias functions (#72)

--- src/Analyzer/Pass/Expression/FunctionCall/AliasCheck.php
+++ src/Analyzer/Pass/Expression/FunctionCall/AliasCheck.php
@@ -14,7 +14,18 @@ class AliasCheck extends AbstractFunctionCallAnalyzer
 {
     protected $map = array(
         'join' => 'implode',
-        'sizeof' => 'count'
+        'sizeof' => 'count',
+        'pos' => 'current',
+        'strchr' => 'strstr',
+        'show_source' => 'highlight_file',
+        'key_exists' => 'array_key_exists',
+        'is_real' => 'is_float',
+        'is_double' => 'is_float',
+        'is_integer' => 'is_int',
+        'is_long' => 'is_int',
+        'ini_alter' => 'ini_set',
+        'fputs' => 'fwrite',
+        'chop' => 'rtrim'
     );
 
     public function pass(FuncCall $funcCall, Context $context)
