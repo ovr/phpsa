27a0fd25670bafe170f3469e24b6f1a09ed5e235
Dmitry Patsura <talk@dmtry.me>
2016-06-03 23:42:32 +0900

    [Analyzer] Optimize DeprecatedFunctions

--- src/Analyzer/Pass/Expression/FunctionCall/DeprecatedFunctions.php
+++ src/Analyzer/Pass/Expression/FunctionCall/DeprecatedFunctions.php
@@ -9,7 +9,7 @@ use PHPSA\Context;
 
 class DeprecatedFunctions extends AbstractFunctionCallAnalyzer
 {
-    protected $map = array(
+    protected $map = [
         'datefmt_set_timezone_id' => ['5.5','IntlDateFormatter::setTimeZone()'],
         'define_syslog_variables' => ['5.3','_'],
         'set_magic_quotes_runtime' => ['5.3','_'],
@@ -24,29 +24,31 @@ class DeprecatedFunctions extends AbstractFunctionCallAnalyzer
         'session_is_registered' => ['5.3','$_SESSION'],
         'session_unregister' => ['5.3','$_SESSION'],
         'session_register' => ['5.3','$_SESSION'],
-    );
+    ];
 
     public function pass(FuncCall $funcCall, Context $context)
     {
         $functionName = $this->resolveFunctionName($funcCall, $context);
-        if ($functionName && isset($this->map[$functionName])) {
-            $context->notice(
-                'deprecated.function',
-                sprintf('%s() is deprecated since PHP %s. Use %s instead.', $functionName, $this->map[$functionName][0], $this->map[$functionName][1]),
-                $funcCall
-            );
-        } elseif ($functionName && substr($functionName, 0, 6) === "mysql_") {
-            $context->notice(
-                'deprecated.function',
-                sprintf('The MySQL Extension is deprecated since PHP 5.5. Use PDO instead.'),
-                $funcCall
-            );
-        } elseif ($functionName && substr($functionName, 0, 7) === "mcrypt_") {
-            $context->notice(
-                'deprecated.function',
-                sprintf('The Mcrypt Extension is deprecated since PHP 7.1. Use paragonie/halite instead.'),
-                $funcCall
-            );
+        if ($functionName) {
+            if (isset($this->map[$functionName])) {
+                $context->notice(
+                    'deprecated.function',
+                    sprintf('%s() is deprecated since PHP %s. Use %s instead.', $functionName, $this->map[$functionName][0], $this->map[$functionName][1]),
+                    $funcCall
+                );
+            } elseif (substr($functionName, 0, 6) === 'mysql_') {
+                $context->notice(
+                    'deprecated.function',
+                    sprintf('The MySQL Extension is deprecated since PHP 5.5. Use PDO instead.'),
+                    $funcCall
+                );
+            } elseif (substr($functionName, 0, 7) === 'mcrypt_') {
+                $context->notice(
+                    'deprecated.function',
+                    sprintf('The Mcrypt Extension is deprecated since PHP 7.1. Use paragonie/halite instead.'),
+                    $funcCall
+                );
+            }
         }
     }
 }
