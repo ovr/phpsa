3505cc02d5427e8149528cecd9225e9b90804805
Dmitry Patsura <talk@dmtry.me>
2016-06-07 13:41:19 +0900

    Fix error on PHP nightly

--- tests/PHPSA/Expression/Operators/UnaryPlusTest.php
+++ tests/PHPSA/Expression/Operators/UnaryPlusTest.php
@@ -41,7 +41,7 @@ class UnaryPlusTest extends \Tests\PHPSA\TestCase
 
         $this->assertInstanceOfCompiledExpression($compiledExpression);
         $this->assertSame(CompiledExpression::INTEGER, $compiledExpression->getType());
-        $this->assertSame(+$value, $compiledExpression->getValue());
+        $this->assertSame(+ (int) $value, $compiledExpression->getValue());
     }
 
     /**
