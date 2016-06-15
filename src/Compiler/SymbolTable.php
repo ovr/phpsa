f171c85966ed2092879d3086559b91a6eb37d51a
Dmitry Patsura <talk@dmtry.me>
2016-06-15 22:29:52 +0900

    Initial commit for SymbolTable, refs #81

---
+++ src/Compiler/SymbolTable.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSA\Compiler;
+
+use Countable;
+use PHPSA\Variable;
+
+class SymbolTable implements Countable
+{
+    /**
+     * @var Variable[]
+     */
+    protected $variables = [];
+
+    /**
+     * @param Variable $variable
+     */
+    public function add(Variable $variable)
+    {
+        $this->variables[$variable->getName()] = $variable;
+    }
+
+    /**
+     * @param string $name
+     * @return Variable|null
+     */
+    public function get($name)
+    {
+        if (isset($this->variables[$name])) {
+            return $this->variables[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->variables);
+    }
+
+    /**
+     * Clear symbol table
+     */
+    public function clear()
+    {
+        $this->variables = [];
+    }
+}
