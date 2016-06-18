987d4d96c6a2ea023822bf545f418cabb3eb7784
Dmitry Patsura <talk@dmtry.me>
2016-06-18 20:25:29 +0900

    [Analyzer] Start working on new Analyzer - Magick methods parameters

---
+++ src/Analyzer/Pass/Statement/MagicMethod/GetParametersCheck.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace PHPSA\Analyzer\Pass\Statement;
+
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPSA\Analyzer\Pass\AnalyzerPassInterface;
+use PHPSA\Analyzer\Pass\ConfigurablePassInterface;
+use PHPSA\Check;
+use PHPSA\Context;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class GetParametersCheck implements ConfigurablePassInterface, AnalyzerPassInterface
+{
+    public function __clone($name, $value)
+    {
+
+    }
+
+    /**
+     * @param ClassMethod $methodStmt
+     * @param Context $context
+     * @return bool
+     */
+    public function pass(ClassMethod $methodStmt, Context $context)
+    {
+        if ($methodStmt->name == '__get') {
+            if (count($methodStmt->params) == 0) {
+                $context->notice(
+                    'magic.get.wrong-parameters',
+                    'Magic method __get must take 1 paramter at least',
+                    $methodStmt,
+                    Check::CHECK_SAFE
+                );
+            }
+        }
+
+        if ($methodStmt->name == '__set') {
+            if (count($methodStmt->params) == 0) {
+                $context->notice(
+                    'magic.get.wrong-parameters',
+                    'Magic method __set must take 1 paramter at least',
+                    $methodStmt,
+                    Check::CHECK_SAFE
+                );
+            }
+        }
+
+        if ($methodStmt->name == '__clone') {
+            if (count($methodStmt->params) > 0) {
+                $context->notice(
+                    'magic.get.wrong-parameters',
+                    'Magic method __clone cannot accept arguments',
+                    $methodStmt,
+                    Check::CHECK_SAFE
+                );
+            }
+        }
+    }
+
+    /**
+     * @return TreeBuilder
+     */
+    public function getConfiguration()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('method_cannot_return')
+            ->canBeDisabled()
+        ;
+
+        return $treeBuilder;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRegister()
+    {
+        return [
+            \PhpParser\Node\Stmt\ClassMethod::class
+        ];
+    }
+}
