fb1024afbe409f53bad322dff731de91b2ee649a
ddmler <eddmler@gmail.com>
2016-08-12 11:32:49 +0200

    Added error suppression analyzer

---
+++ tests/analyze-fixtures/ErrorSuppression.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Compiling\Statements;
+
+class ErrorSuppression
+{
+    /**
+     * @return array
+     */
+    public function testErrorSuppression()
+    {
+        return @in_array(1,[2,3]);
+    }
+
+    /**
+     * @return array
+     */
+    public function testNoErrorSuppression()
+    {
+        return in_array(1,[2,3]);
+    }
+}
+?>
+----------------------------
+[
+    {
+        "type":"error.suppression",
+        "message":"Please don't suppress errors with the @ operator.",
+        "file":"ErrorSuppression.php",
+        "line":11
+    }
+]
