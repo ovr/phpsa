673b44d2083386f87f3c018bc0bc110213ee6f1e
Enrico <eddmler@gmail.com>
2016-09-06 12:58:11 +0200

    Add tests for MethodCannotReturn Analyzer

---
+++ tests/analyze-fixtures/MethodCannotReturn.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Compiling\Statements;
+
+class MethodCannotReturn
+{
+    public function __construct()
+    {
+        return 1;
+    }
+
+    public function __destruct()
+    {
+        return 1;
+    }
+}
+
+class TestNoReturn
+{
+	public $a;
+
+	public function __construct()
+	{
+		$a = 1;
+	}
+
+	public function __destruct()
+	{
+		$a = 0;
+	}
+}
+
+?>
+----------------------------
+[
+    {
+        "type":"return.construct",
+        "message":"Method __construct cannot return a value.",
+        "file":"MethodCannotReturn.php",
+        "line":8
+    },
+    {
+        "type":"return.construct",
+        "message":"Method __destruct cannot return a value.",
+        "file":"MethodCannotReturn.php",
+        "line":13
+    }
+]
