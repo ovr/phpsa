05838f748c227692ddea827067703fedf4d61cc7
Kévin Gomez <contact@kevingomez.fr>
2016-09-06 22:22:44 +0200

    Simplify ArgumentUnpacking

--- src/Analyzer/Pass/Expression/FunctionCall/ArgumentUnpacking.php
+++ src/Analyzer/Pass/Expression/FunctionCall/ArgumentUnpacking.php
@@ -9,22 +9,24 @@ use PHPSA\Definition\FunctionDefinition;
 
 class ArgumentUnpacking extends AbstractFunctionCallAnalyzer
 {
-
     public function pass(FuncCall $funcCall, Context $context)
     {
         $functionName = $this->resolveFunctionName($funcCall, $context);
-        if ($functionName === 'func_get_args') {
-            $scopePointer = $context->scopePointer->getObject();
 
-            if ($scopePointer instanceof ClassMethod || $scopePointer instanceof FunctionDefinition) {
-                if (count($scopePointer->getParams()) === 0) {
-                    $context->notice(
-                        'fcall.argumentunpacking',
-                        sprintf('Please use argument unpacking (...) instead of func_get_args().'),
-                        $funcCall
-                    );
-                    return;
-                }
+        if ($functionName !== 'func_get_args') {
+            return;
+        }
+
+        $scopePointer = $context->scopePointer->getObject();
+
+        if ($scopePointer instanceof ClassMethod || $scopePointer instanceof FunctionDefinition) {
+            if (count($scopePointer->getParams()) === 0) {
+                $context->notice(
+                    'fcall.argumentunpacking',
+                    sprintf('Please use argument unpacking (...) instead of func_get_args().'),
+                    $funcCall
+                );
+                return;
             }
         }
     }
