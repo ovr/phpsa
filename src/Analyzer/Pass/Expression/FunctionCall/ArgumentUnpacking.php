58e7150c4182fa425079395be447bbef74936b65
Kévin Gomez <contact@kevingomez.fr>
2016-09-06 22:21:51 +0200

    Remove unnecessary double quotes

--- src/Analyzer/Pass/Expression/FunctionCall/ArgumentUnpacking.php
+++ src/Analyzer/Pass/Expression/FunctionCall/ArgumentUnpacking.php
@@ -13,7 +13,7 @@ class ArgumentUnpacking extends AbstractFunctionCallAnalyzer
     public function pass(FuncCall $funcCall, Context $context)
     {
         $functionName = $this->resolveFunctionName($funcCall, $context);
-        if ($functionName === "func_get_args") {
+        if ($functionName === 'func_get_args') {
             $scopePointer = $context->scopePointer->getObject();
 
             if ($scopePointer instanceof ClassMethod || $scopePointer instanceof FunctionDefinition) {
