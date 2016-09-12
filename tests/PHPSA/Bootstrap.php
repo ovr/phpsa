86cba92ff2ef739dde1095891a450a99c8e03868
Tom Rochette <roctom@gmail.com>
2016-09-11 23:54:05 -0400

    Use autoload-dev instead of a bootstrap file for phpunit.

--- tests/PHPSA/Bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once __DIR__ . '/../../vendor/autoload.php';
-include_once __DIR__ . '/TestCase.php';
-include_once __DIR__ . '/Expression/Operators/Comparison/BaseTestCase.php';
-include_once __DIR__ . '/Expression/Operators/Arithmetical/AbstractDivMod.php';
