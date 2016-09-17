ffa315fdf9196dd82fe79c584659dc2af9a13bbb
Enrico <eddmler@gmail.com>
2016-09-17 17:30:22 +0200

    Implement Spaceship operator

---
+++ tests/PHPSA/Expression/BinaryOp/SpaceShipTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\PHPSA\Expression\BinaryOp;
+
+use PhpParser\Node;
+use PHPSA\CompiledExpression;
+use PHPSA\Compiler\Expression;
+
+class SpaceShipTest extends \Tests\PHPSA\TestCase
+{
+    /**
+     * @return array
+     */
+    public function getDataProvider()
+    {
+        return array(
+            array(1, 1, 0),
+            array(1, 2, -1),
+            array(2, 1, 1),
+            array(true, false, 1),
+            array(true, 0, 1),
+            array(array(), array(), 0),
+            array(true, array(), 1),
+            array("a", "b", -1),
+            array("a", 2, -1),
+        );
+    }
+
+    /**
+     * Tests {expr} <=> {expr} = {expr}
+     *
+     * @dataProvider getDataProvider
+     */
+    public function testSimpleSuccessCompile($a, $b, $c)
+    {
+        $baseExpression = new Node\Expr\BinaryOp\Spaceship(
+            $this->newScalarExpr($a),
+            $this->newScalarExpr($b)
+        );
+        $compiledExpression = $this->compileExpression($baseExpression);
+
+        $this->assertInstanceOfCompiledExpression($compiledExpression);
+        $this->assertSame(CompiledExpression::INTEGER, $compiledExpression->getType());
+        $this->assertSame($c, $compiledExpression->getValue());
+    }
+
+    public function testUnexpectedTypeFirstArg()
+    {
+        $baseExpression = new Node\Expr\BinaryOp\Spaceship(
+            $this->newFakeScalarExpr(),
+            $this->newScalarExpr(1)
+        );
+        $compiledExpression = $this->compileExpression($baseExpression);
+
+        $this->assertInstanceOfCompiledExpression($compiledExpression);
+        $this->assertSame(CompiledExpression::UNKNOWN, $compiledExpression->getType());
+        $this->assertSame(null, $compiledExpression->getValue());
+    }
+
+    public function testUnexpectedTypeSecondArg()
+    {
+        $baseExpression = new Node\Expr\BinaryOp\Spaceship(
+            $this->newScalarExpr(1),
+            $this->newFakeScalarExpr()
+        );
+        $compiledExpression = $this->compileExpression($baseExpression);
+
+        $this->assertInstanceOfCompiledExpression($compiledExpression);
+        $this->assertSame(CompiledExpression::UNKNOWN, $compiledExpression->getType());
+        $this->assertSame(null, $compiledExpression->getValue());
+    }
+}
