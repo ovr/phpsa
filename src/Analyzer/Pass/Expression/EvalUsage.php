ce97f3338134b0aa43b14267ee01314f4584fcbd
Kévin Gomez <kgomez@tea-ebook.com>
2016-09-25 15:30:31 +0200

    Add an analyzer to discourage eval usage

---
+++ src/Analyzer/Pass/Expression/EvalUsage.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author Kévin Gomez https://github.com/K-Phoen <contact@kevingomez.fr>
+ */
+
+namespace PHPSA\Analyzer\Pass\Expression;
+
+use PhpParser\Node\Expr;
+use PHPSA\Analyzer\Pass\AnalyzerPassInterface;
+use PHPSA\Context;
+
+class EvalUsage implements AnalyzerPassInterface
+{
+    /**
+     * @param Expr\Eval_ $expr
+     * @param Context $context
+     * @return bool
+     */
+    public function pass(Expr\Eval_ $expr, Context $context)
+    {
+        $context->notice(
+            'eval_usage',
+            'Using eval is discouraged.',
+            $expr
+        );
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRegister()
+    {
+        return [
+            Expr\Eval_::class
+        ];
+    }
+}
