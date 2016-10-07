d62ec10fd30d39a49093d83056aa446d7ca70473
Nikhil Saldanha <nikhil.saldanha691@gmail.com>
2016-10-07 19:34:36 +0530

    Added expression analyzer for backtick operator

---
+++ src/Analyzer/Pass/Expression/BacktickUsage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHPSA\Analyzer\Pass\Expression;
+
+use PhpParser\Node\Expr;
+use PHPSA\Analyzer\Pass\AnalyzerPassInterface;
+use PHPSA\Context;
+
+class BacktickUsage implements AnalyzerPassInterface
+{
+    /**
+     * @param Expr\ShellExec $expr
+     * @param Context $context
+     * @return bool
+     */
+    public function pass(Expr\ShellExec $expr, Context $context)
+    {
+        $context->notice(
+            'backtick_usage',
+            'It\'s bad practice to use the backtick operator for shell execution',
+            $expr
+        );
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRegister()
+    {
+        return [
+            Expr\ShellExec::class
+        ];
+    }
+}
