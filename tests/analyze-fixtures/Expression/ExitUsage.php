5cd85aceff1728d40a887493230b461b9fb48a41
Kévin Gomez <kgomez@tea-ebook.com>
2016-10-10 15:59:38 +0200

    Add tests for the exit-usage analyzer

---
+++ tests/analyze-fixtures/Expression/ExitUsage.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Analyze\Fixtures\Expression;
+
+class ExitUsage
+{
+    /**
+     * @return void
+     */
+    public function exitUsageShouldBeDetected()
+    {
+        exit('stop');
+    }
+
+    /**
+     * @return void
+     */
+    public function dieUsageShouldBeDetected()
+    {
+        die('stop');
+    }
+
+    /**
+     * @return array
+     */
+    public function everythingIsOkayHere()
+    {
+        return [
+            0 => 42,
+            1 => 43,
+        ];
+    }
+}
+?>
+----------------------------
+[
+    {
+        "type":"exit_usage",
+        "message":"exit/die statements make the code hard to test and should not be used",
+        "file":"ExitUsage.php",
+        "line":11
+    },
+    {
+        "type":"exit_usage",
+        "message":"exit/die statements make the code hard to test and should not be used",
+        "file":"ExitUsage.php",
+        "line":19
+    }
+]
