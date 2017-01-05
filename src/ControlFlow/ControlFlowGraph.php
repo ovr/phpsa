dc2d58d1ebdfb3745c11fb8f0d30886d5308e970
Dmitry Patsura <talk@dmtry.me>
2017-01-06 00:39:30 +0900

    [ControlFlowGraph] Remove unneeded split by passNodes/passNode (without reference parameter this will break CFG)

--- src/ControlFlow/ControlFlowGraph.php
+++ src/ControlFlow/ControlFlowGraph.php
@@ -31,50 +31,45 @@ class ControlFlowGraph
     protected function passNodes(array $nodes, Block $block)
     {
         foreach ($nodes as $stmt) {
-            $this->passNode($stmt, $block);
-        }
-    }
-
-    protected function passNode($stmt, Block $block)
-    {
-        switch (get_class($stmt)) {
-            case \PhpParser\Node\Expr\Assign::class:
-                $this->passAssign($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\Return_::class:
-                $this->passReturn($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\For_::class:
-                $block = $this->passFor($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\If_::class:
-                $block = $this->passIf($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\While_::class:
-                $block = $this->passWhile($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\Do_::class:
-                $block = $this->passDo($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\Throw_::class:
-                $this->passThrow($stmt, $block);
-                break;
-            case \PhpParser\Node\Expr\Exit_::class:
-                $block->addChildren(new Node\ExitNode());
-                break;
-            case \PhpParser\Node\Stmt\Label::class:
-                $block = $this->createNewBlockIfNeeded($block);
-                $block->label = $stmt->name;
-                break;
-            case \PhpParser\Node\Stmt\TryCatch::class:
-                $block = $this->passTryCatch($stmt, $block);
-                break;
-            case \PhpParser\Node\Stmt\Nop::class:
-                // ignore commented code
-                break;
-            default:
-                echo 'Unimplemented ' . get_class($stmt) . PHP_EOL;
-                break;
+            switch (get_class($stmt)) {
+                case \PhpParser\Node\Expr\Assign::class:
+                    $this->passAssign($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\Return_::class:
+                    $this->passReturn($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\For_::class:
+                    $block = $this->passFor($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\If_::class:
+                    $block = $this->passIf($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\While_::class:
+                    $block = $this->passWhile($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\Do_::class:
+                    $block = $this->passDo($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\Throw_::class:
+                    $this->passThrow($stmt, $block);
+                    break;
+                case \PhpParser\Node\Expr\Exit_::class:
+                    $block->addChildren(new Node\ExitNode());
+                    break;
+                case \PhpParser\Node\Stmt\Label::class:
+                    $block = $this->createNewBlockIfNeeded($block);
+                    $block->label = $stmt->name;
+                    break;
+                case \PhpParser\Node\Stmt\TryCatch::class:
+                    $block = $this->passTryCatch($stmt, $block);
+                    break;
+                case \PhpParser\Node\Stmt\Nop::class:
+                    // ignore commented code
+                    break;
+                default:
+                    echo 'Unimplemented ' . get_class($stmt) . PHP_EOL;
+                    break;
+            }
         }
     }
 
